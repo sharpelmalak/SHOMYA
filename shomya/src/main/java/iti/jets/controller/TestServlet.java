0cd2c502eab24ae0ce941f26d0a9d0d012e9e202
SharpelMalak <sharbelmalak@gmail.com>
2024-09-05 23:39:56 +0300

    create servlet test

---
+++ shomya/src/main/java/iti/jets/controller/TestServlet.java
@@ -0,0 +1,20 @@
+package iti.jets.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet(value = "/")
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("first Servlet");
+    }
+}
